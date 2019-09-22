fa36f50c0a855c25c32270890c3b239746fb1e32
michalosinski91 <mosinski@hotmail.co.uk>
2019-09-22 12:15:13 +0100

    revoke refresh token functionality added

--- src/UserResolver.ts
+++ src/UserResolver.ts
@@ -1,10 +1,11 @@
-import { Resolver, Query, Mutation, Arg, ObjectType, Field, Ctx, UseMiddleware } from 'type-graphql'
+import { Resolver, Query, Mutation, Arg, ObjectType, Field, Ctx, UseMiddleware, Int } from 'type-graphql'
 import { hash, compare } from 'bcryptjs'
 import { User } from './entity/User'
 import { MyContext } from './MyContext'
 import { createRefreshToken, createAccessToken } from './auth'
 import { isAuth } from './isAuth'
 import { sendRefreshToken } from './sendRefreshToken'
+import { getConnection } from 'typeorm'
 
 
 @ObjectType()
@@ -75,5 +76,16 @@ export class UserResolver {
         return {
             accessToken: createAccessToken(user)
         }
+    }
+
+    @Mutation(() => Boolean)
+    async revokeRefreshTokensForUser (
+        @Arg('userId', () => Int) userId: number
+    ) {
+        await getConnection()
+            .getRepository(User)
+            .increment({ id: userId}, 'tokenVersion', 1)
+
+        return true
     }
 }
